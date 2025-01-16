2bce90837fdcd3e250675070f12501f0c131ca06
negnij <JNG279@e.ntu.edu.sg>
2025-01-16 21:19:04 +0800

    linked AdminStorePage

--- client/src/pages/Admin/AdminStorePage.tsx
+++ client/src/pages/Admin/AdminStorePage.tsx
@@ -1,11 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ActionCard from "../../components/Admin/ActionCard";
 import { FaEdit, FaCheckSquare } from "react-icons/fa";
 import { AiOutlineDown, AiOutlineRight } from "react-icons/ai";
 import TransactionHistoryCard from "../../components/Admin/TransactionHistoryCard";
 import InventoryCard from "../../components/Admin/InventoryCard";
+import axios from 'utils/axios'
+
+interface InventoryItem {
+  _id: string;
+  name: string;
+  quantity: number;
+  price: number;
+  status: string;
+  image: string;
+  description: string;
+}
+
+interface Transaction {
+  _id: string;
+  item: string;
+  status: "approved" | "pending" | "declined";
+  username: string;
+  updatedAt: string;
+}
 
 const AdminStorePage: React.FC = () => {
+    const [inventory, setInventory] = useState<InventoryItem[]>([]);
+    const [transaction, setTransaction] = useState<Transaction[]>([]);
+
+    useEffect(() => {
+      const fetchInventory = async () => {
+          try {
+              const { data } = await axios.get<InventoryItem[]>("/inventory");
+              setInventory(data);
+          } catch (error) {
+              console.error("Error fetching inventory:", error);
+          }
+      };
+  
+      fetchInventory();
+  }, []);
+
+  useEffect(() => {
+    const fetchApprovedTransaction = async () => {
+        try {
+            const { data } = await axios.get<Transaction[]>("/transaction/status/approved");
+            setTransaction(data);
+        } catch (error) {
+            console.error("Error fetching inventory:", error);
+        }
+    };
+
+    fetchApprovedTransaction();
+}, []);
+  
   const actionItems = [
     {
       id: "manage",
@@ -59,7 +107,15 @@ const AdminStorePage: React.FC = () => {
           <AiOutlineRight style={{ color: "#5E5E5E" }} />
         )}
       </div>
-      {isInventoryExpanded && <InventoryCard />}
+      {isInventoryExpanded && inventory.map((item) => (
+          <InventoryCard
+              key={item._id}
+              name={item.name}
+              id={item._id}
+              quantity={100 - item.quantity} 
+          />
+        ))}
+
       <div
         style={{
           display: "flex",
@@ -76,7 +132,15 @@ const AdminStorePage: React.FC = () => {
           <AiOutlineRight style={{ color: "#5E5E5E" }} />
         )}
       </div>
-      {isHistoryExpanded && <TransactionHistoryCard />}
+      {isHistoryExpanded && transaction.map((item) => (
+          <TransactionHistoryCard
+              key={item._id}
+              item={item.item}
+              id={item._id}
+              user={item.username}
+              date={item.updatedAt}
+          />
+        ))}
     </div>
   );
 };
