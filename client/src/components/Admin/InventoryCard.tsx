0b18abfa6e5594c692e4515c235f5c5e371029f8
jingrongggg <limj0279@e.ntu.edu.sg>
2025-01-17 15:56:47 +0800

    fixed quantity available

--- client/src/components/Admin/InventoryCard.tsx
+++ client/src/components/Admin/InventoryCard.tsx
@@ -12,48 +12,49 @@ const InventoryCard: React.FC<InventoryProps> = ({ id, name, quantity }) => {
         {
           id: id,
           name: name,
-          sold: 100-quantity,
           remaining: quantity,
         },
       ];
 
-      return(
+      return (
         <div>
-      {actionItems.map((item) => (
-        <div
-        style={{
-          border: "1px solid #ccc",
-          borderRadius: "8px",
-          padding: "16px",
-          width: "100%",
-          textAlign: "center",
-        }}
-       >
-        <p style={{ fontWeight: "bold", margin: 0 }}>
-        {item.name}
-        </p>
-        <div
-        style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginTop: "8px",
-        }}
-        >
-        <div style={{ textAlign: "center" }}>
-            <p style={{ margin: 0, fontWeight: "bold" }}>{item.sold}</p>
-            <p style={{ margin: 0, color: "#5E5E5E", fontWeight: "bold" }}>Sold</p>
+          {actionItems.map((item) => (
+            <div
+              key={item.id}
+              style={{
+                border: "1px solid #ccc",
+                borderRadius: "8px",
+                padding: "16px",
+                width: "100%",
+                textAlign: "center",
+                marginBottom: "8px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <p style={{ fontWeight: "bold", margin: 0 }}>{item.name}</p>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginTop: "8px",
+                  flexGrow: 1, // Allows this div to take up available space
+                }}
+              >
+                <p style={{ margin: 0, fontWeight: "bold", fontSize: "24px" }}>
+                  {item.remaining}
+                </p>
+                <p style={{ margin: 0, color: "#5E5E5E", fontWeight: "bold" }}>
+                  Remaining
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
-        <div style={{ height: "40px", width: "1px", background: "#ccc" }} />
-        <div style={{ textAlign: "center" }}>
-            <p style={{ margin: 0, fontWeight: "bold" }}>{item.remaining}</p>
-            <p style={{ margin: 0, color: "#5E5E5E", fontWeight: "bold" }}>Remaining</p>
-        </div>
-        </div>
-        </div>
-      ))}
-    </div>
-
       );
 };
 
